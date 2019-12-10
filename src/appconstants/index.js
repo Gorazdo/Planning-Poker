f68c19ea030636aaf1fa81b415278446390450b8
Pavel Shchegolev <pshchegolev@asset-control.com>
2019-12-10 10:30:21 +0100

    clean up a bit

--- src/appconstants/index.js
+++ src/appconstants/index.js
@@ -3,64 +3,26 @@ export const APP_ID = '3074457347007443177';
 // eslint-disable-next-line no-magic-numbers
 export const CARDS_BACK_INDEXES = [1, 2, 3, 4, 5, 6];
 
-export const CARDS_LIST = [
-	{
-		label: '0',
-		value: 0,
-	},
-	{
-		label: '½',
-		value: 0.5,
-	},
-	{
-		label: '1',
-		value: 1,
-	},
-	{
-		label: '2',
-		value: 2,
-	},
-	{
-		label: '3',
-		value: 3,
-	},
-	{
-		label: '5',
-		value: 5,
-	},
-	{
-		label: '8',
-		value: 8,
-	},
-	{
-		label: '13',
-		value: 13,
-	},
-	{
-		label: '20',
-		value: 20,
-	},
-	{
-		label: '40',
-		value: 40,
-	},
-	{
-		label: '100',
-		value: 100,
-	},
-	{
-		label: '∞',
-		value: 'infinity',
-	},
-	{
-		label: '?',
-		value: 'question',
-	},
-	{
-		label: '☕',
-		value: 'coffee',
-	},
+const CARDS_FRONT_ENTRIES = [
+	['0', 0],
+	['½', 0.5],
+	['1', 1],
+	['2', 2],
+	['3', 3],
+	['5', 5],
+	['8', 8],
+	['13', 13],
+	['20', 20],
+	['40', 40],
+	['100', 100],
+	['∞', 'infinity'],
+	['?', 'question'],
+	['☕', 'coffee'],
 ];
+
+const entriesToObjectMap = ([label, value]) => ({ label, value });
+
+export const CARDS_LIST = CARDS_FRONT_ENTRIES.map(entriesToObjectMap);
 
 export const CARDS_MAP = CARDS_LIST.reduce((acc, item) => {
 	acc[item.value] = item;
--- src/utils/getCardURL.js
+++ src/utils/getCardURL.js
@@ -1,4 +1,2 @@
-const { BASE_URL } = process.env;
-
 export default (side, value = '') =>
 	`${window.location.origin}/assets/cards/${side}${value}.svg`;
